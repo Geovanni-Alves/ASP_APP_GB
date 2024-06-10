ac6e2e684d8173ea77480c723563b5d1ff646369
geovanni-alves <geo-estevam@hotmail.com>
2024-06-09 23:08:26 -0700

    commit

--- userapp/src/lib/supabase.js
+++ userapp/src/lib/supabase.js
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 // const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 // const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 
-const supabaseUrl = "http://127.0.0.1:54321";
+const supabaseUrl = "http://192.168.1.77:54321";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
